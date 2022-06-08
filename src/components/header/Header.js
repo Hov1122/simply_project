976d1064e2da078834387969fd27682ad318cce3
Davit-Avakian <davitavakian05@gmail.com>
2022-06-09 00:25:08 +0400

    header component fix

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,21 +2,31 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import defaultPic from '../../assets/defaultPic.jpg'
+import { useSelector } from 'react-redux'
+import { authSelector } from '../../state-management/auth/selectors'
 import './Header.css'
 
-const Header = () => {
+const Header = ({ setShowMenu }) => {
+
+  const { token } = useSelector(authSelector);
+
   return (
+    token ? 
     <div className='Header-Container'>
         <FontAwesomeIcon icon={faBars} style={{
             color: '#ffff',
             fontSize: '35px',
             margin: 'auto 0'
+        }} onClick={() => {
+          setShowMenu((oldState) => (
+            !oldState
+          ))
         }}/>
         <div className='search-bar-container'>
             <input className='search-bar'/>
         </div>
         <img src={defaultPic} className='profile-picture'/>
-    </div>
+    </div> : null
   )
 }
 
