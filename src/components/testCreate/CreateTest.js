bf475ad23e2b8c15b5186c4d20b771838e324912
haykmadatyan98 <madatyanh@gmail.com>
2022-06-12 17:04:42 +0400

    Test

--- src/components/testCreate/CreateTest.js
+++ src/components/testCreate/CreateTest.js
@@ -5,32 +5,75 @@ import Loading from "../common/Loading";
 function TestCreater() {
   const [loading, setLoading] = useState(false);
 
+  // ADD NEW ANSWER ROW
   const addAnswerRow = (e) => {
     const container = e.target.parentElement.querySelector('.AnswersForm');
     const element = container.querySelector('.Answer')
-
+    const answerNumber = container.querySelectorAll('.Answer').length + 1;
+    
     const newElement = element.cloneNode(true)
-    newElement.querySelector("input[type=radio]")
-    container.append(newElement)
+    const radioButton = newElement.querySelector("input[type=radio]")
+    radioButton.value = answerNumber
+    const answerInput = newElement.querySelector("input[type=text]")
+    console.log(answerInput)
+    answerInput.setAttribute('data-value', 'answer')
+    container.appendChild(newElement)
   }
 
+  
+  // ADD NEW QUESTION
   const addQuestionRow = (e) => {
     const container = e.target.parentElement.querySelector('.QuestionsForm');
     const element = document.querySelector('template').childNodes[0];
-    const questionNumber = document.querySelectorAll('.Question').length + 1
+    element.childNodes[1].setAttribute('data-value', 'questions')
+    const questionNumber = document.querySelectorAll('.Question').length + 1;
 
     const newElement = element.cloneNode(true);
     newElement.querySelector("h3").innerText += questionNumber;
     newElement.classList.add('Question');
-    newElement.setAttribute('data-value', questionNumber)
     newElement.querySelector('input[type=button]').addEventListener('click', addAnswerRow)
-    newElement.querySelector('input[type=radio]').name = `Question${questionNumber}Answer`
+    newElement.querySelector('input[type=radio]').name = `Question${questionNumber - 1}`
+    newElement.querySelector('input[type=radio]').value = 1
 
     container.append(newElement);
   }
 
+  // ADD TEST IN DATABASE
   const addTest = (e) => {
-    console.log(e.target)
+    const testName = document.querySelector('[data-value="testName"]').value
+    const testSubject = document.querySelector('[data-value="testSubject"]').value
+    const testDate = document.querySelector('[data-value="testDate"]').value
+    const testGroup = document.querySelector('[data-value="testGroup"]').value
+    const testQuestions = document.querySelectorAll('[data-value="questions"]')
+    const testAnswers = document.querySelectorAll('[data-value="answer"]')
+    const testRigthAnswers = document.querySelectorAll('[data-value="rigthAnswer"]')
+
+    const questions = [];
+    const answers = [];
+    const rigthAnswers = [];
+
+    testAnswers.forEach(element => {
+      const questionNumber = +element.parentElement.childNodes[0].name.split('Question')[1];
+      if (isNaN(questionNumber)) return
+      else if (typeof answers[questionNumber] !== 'object') answers[questionNumber] = []
+        answers[questionNumber].push(element.value)
+    })
+
+    testQuestions.forEach((element, index) => {if (index) questions.push(element.value)})
+
+    testRigthAnswers.forEach(element => {if (element.checked) rigthAnswers.push(+element.value - 1)})
+
+    const data = {
+      name: testName,
+      subject: testSubject,
+      date: testDate,
+      group: testGroup,
+      questions: questions,
+      answers: answers,
+      rigthanswers: rigthAnswers
+    }
+
+    console.log(data) // back request
   }
 
   if (loading) {
@@ -51,12 +94,12 @@ function TestCreater() {
       </div>
       <div className="QuestionsForm">
         <template>
-          <div data-value='0'>
+          <div>
             <h3>Question</h3>
-            <input placeholder="Enter Question" type='text' data-value='question'/>
+            <input placeholder="Enter Question" type='text'/>
             <div className="AnswersForm">
               <div className="Answer">
-                <input type='radio' name="Question1Answer"/>
+                <input type='radio' name="Question1Answer" data-value='rigthAnswer'/>
                 <input placeholder="Enter Answer" type='text' data-value='answer'/>
               </div>
             </div>
