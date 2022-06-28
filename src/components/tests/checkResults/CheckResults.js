a0400d19cf51fc2041750c95ae83b29f39495c71
Davit-Avakian <davitavakian05@gmail.com>
2022-06-28 16:47:25 +0400

    check results picked answer

--- src/components/tests/checkResults/CheckResults.js
+++ src/components/tests/checkResults/CheckResults.js
@@ -13,7 +13,7 @@ import "./CheckResults.css";
 
 const CheckResults = () => {
   const [questionCount, setQuestionCount] = useState(5);
-  
+
   const {
     state: { testId },
   } = useLocation();
@@ -21,7 +21,7 @@ const CheckResults = () => {
   const {
     user: { id },
   } = useSelector(authSelector);
-  const { testResults, currentTest} = useSelector(testsSelector);
+  const { testResults, currentTest } = useSelector(testsSelector);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -31,61 +31,71 @@ const CheckResults = () => {
 
   return (
     <div className="Check-Results-Container">
-      <h2>{currentTest.name}</h2>
-      {currentTest.questions && testResults.questionMarks && currentTest.questions
-        .slice(questionCount - 5, questionCount)
-        .map(({ id: qid, name, answers }) => {
-          return (
-            <motion.div
-              initial={{ x: "100vw" }}
-              animate={{
-                x: 0,
-                transition: { ease: "easeInOut", duration: 0.5 },
-              }}
-              key={qid}
-              className="Question-Container"
-            >
-              <h3>Question {currentTest.questions.findIndex((q) => q.id === qid) + 1}</h3>
-              <span className="question-text">Mark for question: {testResults.questionMarks[qid]}</span>
-              <span className="question-text">{name}</span>
-              <div className="Answers-Container">
-                {answers.map(({ id: aid, name, isCorrect }) => {
-                  return (
-                    <div key={aid} className="Answer">
-                    {console.log(testResults.questions[qid][0][aid] !== 'undefined' ? 'checked' : false)}
+      <h2>{currentTest?.name}</h2>
+      {currentTest?.questions &&
+        testResults?.questionMarks &&
+        currentTest?.questions
+          .slice(questionCount - 5, questionCount)
+          .map(({ id: qid, name, answers }) => {
+            return (
+              <motion.div
+                initial={{ x: "100vw" }}
+                animate={{
+                  x: 0,
+                  transition: { ease: "easeInOut", duration: 0.5 },
+                }}
+                key={qid}
+                className="Question-Container"
+              >
+                <h3>
+                  Question{" "}
+                  {currentTest?.questions?.findIndex((q) => q.id === qid) + 1}
+                </h3>
+                <span className="question-text">
+                  Mark for question: {testResults?.questionMarks[qid]}
+                </span>
+                <span className="question-text">{name}</span>
+                <div className="Answers-Container">
+                  {answers.map(({ id: aid, name, isCorrect }) => {
+                    return (
+                      <div key={aid} className="Answer">
                         <input
                           className="answer-input"
                           type="checkbox"
                           disabled="disabled"
                           id={aid}
-                          style={
-                            { borderRadius: 10,  
-                              borderColor: (isCorrect ? 'green' : 'red')
-                            }
+                          defaultChecked={
+                            testResults?.questions[qid][0][aid] !== undefined
+                              ? true
+                              : false
                           }
-                          />
-                      <label htmlFor={aid} className="answer-text">
-                        {name}
-                      </label>
-                    </div>
-                  );
-                })}
-              </div>
-            </motion.div>
-          );
-        })}
-        { currentTest.questions &&
+                          style={{
+                            borderRadius: 10,
+                            borderColor: isCorrect ? "green" : "red",
+                          }}
+                        />
+                        <label htmlFor={aid} className="answer-text">
+                          {name}
+                        </label>
+                      </div>
+                    );
+                  })}
+                </div>
+              </motion.div>
+            );
+          })}
       <Pagination
-        count={Math.ceil(currentTest.questions.length / 5)}
+        count={Math.ceil(currentTest?.questions?.length / 5)}
+        sx={{ marginTop: 3 }}
         variant="outlined"
         color="primary"
         size="large"
         onChange={(e, value) => {
           setQuestionCount(value * 5);
         }}
-      />}
+      />
     </div>
-  )
+  );
 };
 
 export default CheckResults;
